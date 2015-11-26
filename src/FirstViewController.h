48c1da076b9f08c46d40920b100d14fc96a5b125
Andrea Franceschini <andrea.franceschini@gmail.com>
2015-11-26 16:38:14 +0100

    iAd done

--- src/FirstViewController.h
+++ src/FirstViewController.h
@@ -1,7 +1,8 @@
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
 #include "cinder/Function.h"
 
-@interface FirstViewController : UINavigationController
+@interface FirstViewController : UINavigationController <ADBannerViewDelegate>
 
 @end
--- src/FirstViewController.mm
+++ src/FirstViewController.mm
@@ -3,6 +3,11 @@
 #include "cinder/app/App.h"
 #include "CircularBellsApp.h"
 
+@interface FirstViewController()
+	@property (nonatomic) BOOL isBannerVisible;
+	@property (strong, nonatomic) ADBannerView* bannerView;
+@end
+
 @implementation FirstViewController
 
 - (void)viewDidLoad {
@@ -15,16 +20,51 @@
 	
 	cinderViewParent.title = @"Circular Bells";
 	[self setNavigationBarHidden:YES];
-//	cinderViewParent.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.infoButton];
-//	cinderViewParent.toolbarItems = [self tabBarItems];
+	//cinderViewParent.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.infoButton];
+	//cinderViewParent.toolbarItems = [self tabBarItems];
+
+	_isBannerVisible = NO;
+	_bannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+	CGRect frame = _bannerView.frame;
+	frame.origin.y = self.view.bounds.size.height;
+	_bannerView.frame = frame;
+	_bannerView.delegate = self;
+	[self.view addSubview:_bannerView];
 }
 
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-	[super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-	
-	[coordinator animateAlongsideTransition:nil completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
-//		ci::app::App::get()->resize();
-	}];
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner {
+	if(!_isBannerVisible) {
+		[UIView animateWithDuration:0.5 animations:^{
+			CGRect frame = _bannerView.frame;
+			frame.origin.y -= frame.size.height;
+			_bannerView.frame = frame;
+		} completion:^(BOOL finished) {
+			_isBannerVisible = YES;
+		}];
+	}
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
+	if(_isBannerVisible) {
+		[UIView animateWithDuration:0.5 animations:^{
+			CGRect frame = _bannerView.frame;
+			frame.origin.y = self.view.bounds.size.height;
+			_bannerView.frame = frame;
+		} completion:^(BOOL finished) {
+			_isBannerVisible = NO;
+		}];
+	}
+}
+
+//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+- (void)viewDidLayoutSubviews {
+	CGSize bigSize = self.view.bounds.size;
+	CGSize newSize = [_bannerView sizeThatFits:bigSize];
+	if(_isBannerVisible) {
+		_bannerView.frame = CGRectMake(0, bigSize.height - newSize.height, newSize.width, newSize.height);
+	} else {
+		_bannerView.frame = CGRectMake(0, bigSize.height, newSize.width, newSize.height);
+	}
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,7 +72,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -40,6 +79,5 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
